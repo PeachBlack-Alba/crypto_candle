9353fa8c5158232aa979811da425577e85bb52e7
PeachBlack-Alba <peachblackweb@gmail.com>
2024-06-17 15:57:19 +0200

    initial set up for project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Wallet.h"
+#include <iostream>
+#include "MerkelMain.h"
+#include "Candlestick.h"
+
+
+int main()
+{   
+   // MerkelMain app{};
+   // app.init();
+    std::cout << "Candlestick project setup complete!" << std::endl;
+    return 0;
+
+}
